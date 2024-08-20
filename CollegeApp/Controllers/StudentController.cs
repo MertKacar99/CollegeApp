abf9ba3003834381729998f48ec7c2798e0ba638
Mert <mertkacar5800@gmail.com>
2024-08-20 17:05:49 +0300

    updated StudentController class.

--- CollegeApp/Controllers/StudentController.cs
+++ CollegeApp/Controllers/StudentController.cs
@@ -10,59 +10,71 @@ namespace CollegeApp.Controllers
     {
 
         [HttpGet]
-        [Route("All",Name = "GetAllStudent")]
-        public IEnumerable<Student> GetStudentStudent()
+        [Route("All", Name = "GetAllStudent")]
+        public ActionResult<IEnumerable<Student>> GetStudentStudent()
         {
-            return  CollegeRepository.Students; 
+            // OK - 200 - SUCCESS
+            return Ok(CollegeRepository.Students);
         }
 
-        [HttpGet]       
-        [Route("{id:int}",Name = "GetStudentById")]
-        public Student? GetStudentById(int id)
+        [HttpGet]
+        [Route("{id:int}", Name = "GetStudentById")]
+        public ActionResult<Student?> GetStudentById(int id)
         {
-            return CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
+            //
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            return Ok(CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault());
         }
         [HttpGet]
-        [Route("{name:alpha}",Name = "GetStudentByName")]
-        public Student? GetStudentByName(string name)
+        [Route("{name:alpha}", Name = "GetStudentByName")]
+        public ActionResult<Student?> GetStudentByName(string name)
         {
             // return CollegeRepository.Students.Where(x => x.Name.ToLower() == name.ToLower()).FirstOrDefault();
-            return CollegeRepository.Students.Where(x => x.Name == name).FirstOrDefault();
+            return Ok(CollegeRepository.Students.Where(x => x.Name == name).FirstOrDefault());
         }
 
         [HttpPost]
-        [Route("Add",Name = "AddStudent")]
-        public Student AddStudent(Student student)
+        [Route("Add", Name = "AddStudent")]
+        public ActionResult<Student> AddStudent(Student student)
         {
             CollegeRepository.Students.Add(student);
-            return student;
+            return Ok(student);
         }
 
-        [HttpDelete("{id}",Name = "DeleteStudent")]
-        public bool DeleteStudent(int id){
+        [HttpDelete("{id}", Name = "DeleteStudent")]
+        public ActionResult<bool> DeleteStudent(int id)
+        {
             var student = CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
-            if(student != null){
-            CollegeRepository.Students.Remove(student);
-            return true;
+            if (student != null)
+            {
+                CollegeRepository.Students.Remove(student);
+                return Ok(true);
             }
-            return false;
+            return NotFound(false);
         }
 
-        [HttpPut("{id}",Name = "UpdateStudent")]
-        public bool UpdateStudent(int id, Student student){
-            var std = CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
-            if(std != null){
-                std.Name = student.Name;
-                std.Surname = student.Surname;
-                std.Address= student.Address;
-                std.email = student.email;
-                std.BirthYear= student.BirthYear;
-                Console.WriteLine(  std.Id + "'li  Öğrenci güncellendi"+std);
-                return true;
+        [HttpPut("{id}", Name = "UpdateStudent")]
+        public ActionResult<bool> UpdateStudent(int id, Student student)
+        {
+            var std = CollegeRepository.Students.FirstOrDefault(x => x.Id == id);
+
+            if (std == null)
+            {
+                return NotFound(false);
             }
-            return false;
+
+            std.Name = student.Name;
+            std.Surname = student.Surname;
+            std.Address = student.Address;
+            std.email = student.email;
+            std.BirthYear = student.BirthYear;
+
+            Console.WriteLine($"Öğrenci {std.Id} güncellendi: {std}");
+
+            return Ok(true);
         }
-
-
     }
 }
